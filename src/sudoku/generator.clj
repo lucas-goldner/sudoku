153d5446caf15d540ae322a9828938fe34d25947
lucas-goldner <lucas.goldner@googlemail.com>
2023-07-25 00:19:27 +0200

    feat: Go back to basics

--- src/sudoku/generator.clj
+++ src/sudoku/generator.clj
@@ -1,5 +1,5 @@
-(ns sudoku.generator
-  (:require [clojure.core.matrix :as m]))
+;; From https://github.com/baskeboler/clj-sudoku/blob/master/src/clj_sudoku/core.clj
+(ns sudoku.generator)
 
 (defn base-sudoku []
   "Returns a valid solved 9x9 sudoku puzzle."
@@ -12,7 +12,6 @@
    [1 5 7 4 9 2 6 8 3]
    [6 9 4 7 3 8 2 1 5]
    [3 2 8 5 6 1 7 4 9]])
-
 
 (defn print-sudoku [sudoku]
   "Prints a sudoku to the console with indices e.g: A B C for columns and 1 2 3 rows"
@@ -27,28 +26,7 @@
         (print cell " "))
       (println))))
 
-(defn get-3x3-blocks [sudoku]
-  "Returns blocks of 3x3 from the given sudoku."
-  (let [blocks (partition 3 (map (partial partition 3) sudoku))]
-    (apply concat (map #(mapv vec %) blocks))))
-
-(defn shuffle-row-blocks [sudoku]
-  "Shuffles the row-blocks of the sudoku while maintaining its validity."
-  (let [blocks (partition 3 (map (partial partition 3) sudoku))
-        shuffled-blocks (->> blocks
-                             (shuffle)
-                             (get-3x3-blocks))]
-    shuffled-blocks))
-
-(defn transform-sudoku [sudoku]
-  (let [blocks (partition 3 sudoku)]
-    (->> (reverse blocks)
-         (mapv #(apply concat %))
-         (mapv #(->> % (partition 3) (mapv (apply concat %)))))))
-
 (defn create-sudoku [x]
   "Generates and prints a valid shuffled 9x9 sudoku."
   (-> (base-sudoku)
-      shuffle-row-blocks
-      transform-sudoku
       print-sudoku))
