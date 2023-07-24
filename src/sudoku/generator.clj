a5ae4b0cb64fabe62ee644592c80b80ab4ef2867
lucas-goldner <lucas.goldner@googlemail.com>
2023-07-24 20:27:24 +0200

    feat: Print encoding

--- src/sudoku/generator.clj
+++ src/sudoku/generator.clj
@@ -6,12 +6,19 @@
   (map (fn [_] (vec (range 1 (inc x)))) (range x)))
 
 
-(defn print-sudoku "Prints a sudoku to the console"
+(defn print-sudoku
+  "Prints a sudoku to the console with indices e.g: A B C for columns and 1 2 3 rows"
   [sudoku]
-  (doseq [row sudoku]
-    (doseq [cell row]
-      (print cell " "))
-    (println)))
+  (let [num-cols (count (first sudoku))]
+    (print "   ")
+    (doseq [col-label (range 1 (inc num-cols))]
+      (print (char (+ 64 col-label)) " "))
+    (println)
+    (doseq [[row-label row] (map vector (range 1 (inc (count sudoku))) sudoku)]
+      (print row-label " ")
+      (doseq [cell row]
+        (print cell " "))
+      (println))))
 
 (defn create-sudoku
   "Creates a sudoku of size x by gradually expanding the sudoku vector"
